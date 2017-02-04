4ab74787f51473dd0150550ccb3da47ccb59782f
Erik Tempelaar <erik.tempelaar@altran.com>
2017-02-04 18:49:53 +0100

    Configure meld as diff-tool

--- vagrant/provision/provision.sh
+++ vagrant/provision/provision.sh
@@ -13,3 +13,11 @@ sudo apt-get -y install clang
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
 
+echo -----------------------------------
+echo INSTALL MELD
+echo -----------------------------------
+sudo apt-get -y install meld
+echo "#! /bin/sh" > /usr/local/bin/git-meld
+echo "meld $2 $5" >>  /usr/local/bin/git-meld
+chown vagrant:vagrant /usr/local/bin/git-meld
+chmod u+x /usr/local/bin/git-meld
