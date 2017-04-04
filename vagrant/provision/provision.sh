47ff25aced2f9d4c14f883d2f5c9162692ae653f
Erik Tempelaar <erik.tempelaar@altran.com>
2017-04-04 20:59:49 +0200

    Add Visual Code to provisioning

--- vagrant/provision/provision.sh
+++ vagrant/provision/provision.sh
@@ -1,3 +1,11 @@
+echo -----------------------------------
+echo PREPARE VSCODE
+echo -----------------------------------
+sudo apt-get -y install curl
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
 apt-get update && apt-get -y apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" upgrade && apt-get -y autoremove
 
 echo -----------------------------------
@@ -21,3 +29,12 @@ echo "#! /bin/sh" > /usr/local/bin/git-meld
 echo "meld $2 $5" >>  /usr/local/bin/git-meld
 chown vagrant:vagrant /usr/local/bin/git-meld
 chmod u+x /usr/local/bin/git-meld
+
+echo -----------------------------------
+echo INSTALL VSCODE
+echo -----------------------------------
+sudo apt-get -y install code
+
+
+
+
